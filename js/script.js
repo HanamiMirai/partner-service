c72a4b8fa10908d61d070aa2afcff5452f95c0b3
Hanami <64640200+HanamiMirai@users.noreply.github.com>
2022-06-04 23:35:51 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function burgerMenu(selector){
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__btn');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__over');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('active');
+
+        if (menu.hasClass('active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
+
+var wow = new WOW(
+    {
+      boxClass: 'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset: 0,          // distance to the element when triggering the animation (default is 0)
+      mobile: true,       // trigger animations on mobile devices (default is true)
+      live: true,       // act on asynchronously loaded content (default is true)
+      callback: function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
+    }
+  );
+  wow.init();
